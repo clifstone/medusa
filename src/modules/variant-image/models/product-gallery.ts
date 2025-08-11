19f2b92a3e8ab8f7613e6c672b8b08ac0f59f27c
clifstone <kipreinwand@gmail.com>
2025-08-11 00:05:21 -0500

    add categorized product images

--- src/modules/variant-image/models/product-gallery.ts
+++ src/modules/variant-image/models/product-gallery.ts
@@ -3,7 +3,12 @@ import { model } from "@medusajs/framework/utils"
 export const ProductGallery = model.define("product_gallery", {
   id: model.id().primaryKey(),
   product_id: model.text(), // FK value to the product (no cross-module relationship)
-  orientation: model.enum(["square", "portrait", "landscape", "full"]),
+  orientation: model.enum([
+    "square",
+    "portrait",
+    "landscape",
+    "full",
+  ]),
   url: model.text(),
 })
 
